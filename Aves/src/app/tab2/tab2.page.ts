3dd2dde5602bc9ddc710935246850e006a4cc01b
JCamilo23 <jcecheverry09@gmail.com>
2024-11-23 10:58:46 -0500

    Tabla2Camilo

--- Aves/src/app/tab2/tab2.page.ts
+++ Aves/src/app/tab2/tab2.page.ts
@@ -3,10 +3,33 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  // Variables para capturar las selecciones
+  tipoHabitat: string = '';
+  clima: string = '';
+  vegetacion: string = '';
+
+  // Array para guardar los datos en la tabla
+  datos: Array<{ tipoHabitat: string; clima: string; vegetacion: string }> = [];
 
   constructor() {}
 
+  // Función para guardar los datos seleccionados
+  guardarSeleccion() {
+    if (this.tipoHabitat && this.clima && this.vegetacion) {
+      this.datos.push({
+        tipoHabitat: this.tipoHabitat,
+        clima: this.clima,
+        vegetacion: this.vegetacion,
+      });
+      // Limpiar los selectores después de guardar
+      this.tipoHabitat = '';
+      this.clima = '';
+      this.vegetacion = '';
+    } else {
+      alert('Por favor selecciona todas las opciones antes de guardar.');
+    }
+  }
 }
