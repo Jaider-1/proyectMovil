0d8c848b5374e9e4a1a56c1dd10559f08bb43f1f
Alekay117 <lobalekay19@gmail.com>
2024-11-25 20:09:00 -0500

    f

--- Aves/src/app/tab1/tab1.page.ts
+++ Aves/src/app/tab1/tab1.page.ts
@@ -1,58 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-aves',
-  templateUrl: './tab1.page.html',
-  styleUrls: ['./tab1.page.sccs'],
+  selector: 'app-tab4',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
 })
-export class AvesPage {
-  // Listado de aves
-  aves: { id: number; nombreComun: string; nombreCientifico: string }[] = [
-    { id: 1, nombreComun: 'Colibrí', nombreCientifico: 'Trochilidae' },
-    { id: 2, nombreComun: 'Cóndor', nombreCientifico: 'Vultur gryphus' },
-  ];
-
-  // Nueva ave para agregar
-  nuevoAve = {
-    nombreComun: '',
-    nombreCientifico: '',
-  };
-
-  mostrarFormulario = false;
+export class Tab4Page {
 
   constructor() {}
 
-  // Abre el formulario modal
-  abrirFormulario() {
-    this.mostrarFormulario = true;
-  }
-
-  // Cierra el formulario modal
-  cerrarFormulario() {
-    this.mostrarFormulario = false;
-    this.limpiarFormulario();
-  }
-
-  // Agrega un nuevo ave al listado
-  agregarAve() {
-    if (this.nuevoAve.nombreComun && this.nuevoAve.nombreCientifico) {
-      const nuevoId = this.aves.length > 0 ? this.aves[this.aves.length - 1].id + 1 : 1;
-      this.aves.push({
-        id: nuevoId,
-        nombreComun: this.nuevoAve.nombreComun,
-        nombreCientifico: this.nuevoAve.nombreCientifico,
-      });
-      this.cerrarFormulario();
-    } else {
-      alert('Por favor, complete todos los campos.');
-    }
-  }
-
-  // Limpia los datos del formulario
-  limpiarFormulario() {
-    this.nuevoAve = {
-      nombreComun: '',
-      nombreCientifico: '',
-    };
-  }
 }
